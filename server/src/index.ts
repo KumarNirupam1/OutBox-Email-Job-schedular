d7bb687ce91daa401b26156720ebb4d1f5fcb23b
Kumar Nirupam <kumar.nirupam24@gmail.com>
2026-09-02 01:11:11 +0530

    added Oauth

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,23 @@
 import "dotenv/config";
 import express from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth.js';
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT ?? 8080;
+const clientUrl = process.env.CLIENT_URL ?? "https://localhost:3000";
+
+app.use(
+    cors({
+        origin : clientUrl,
+        credentials:true,
+    })
+)
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+
 
 app.use(express.json());
 
---
+++ server/src/lib/auth.ts
@@ -0,0 +1,21 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+// If your Prisma file is located elsewhere, you can change the path
+import prisma from './db.js'
+
+const clientUrl = process.env.BETTER_AUTH_URL || `http://localhost:3000`
+
+export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL,
+    secret: process.env.BETTER_AUTH_SECRET,
+    trustedOrigins: [clientUrl],
+    database: prismaAdapter(prisma, {
+        provider: "postgresql", // or "mysql", "postgresql", ...etc
+    }),
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        },
+    }
+});
